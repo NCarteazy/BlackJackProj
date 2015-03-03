397494605d4e9453891615dfc04f17a5158beb6b
NickC <carter.nicholas.r@gmail.com>
2015-03-02 16:16:52 -0800

    yuio

---
+++ BlackJack/BlackJack/BlackJack.cpp
@@ -0,0 +1,17 @@
+// BlackJack.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	
+	cout << "Hello World" << endl;
+	Deck a;
+	cout << a;
+
+	cin.ignore();
+	return 0;
+}
+
